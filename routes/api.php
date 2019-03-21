32c73225b1bc1a97e8834884a3e60d1fbe526b4d
Julien Caramazza <julien.caramazza@gmail.com>
2019-03-21 16:25:12 +0100

    manque register

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,9 @@ Route::delete('/albums/{album}', 'AlbumController@destroy')->name('albums.destro
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', 'AuthController@register');
+
+Route::post('/login', 'AuthController@login');
+
+Route::post('/logout', 'AuthController@logout');
